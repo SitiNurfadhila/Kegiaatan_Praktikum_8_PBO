d3c4491f8870fa4550b399bcc0b8987da67bea40
SitiNurfadhila <127620903+SitiNurfadhila@users.noreply.github.com>
2023-05-18 21:32:01 -0700

    Add files via upload

---
+++ book/BookService.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Book;
+
+import java.util.List;
+
+/**
+ *
+ * @author Lenovo
+ */
+    public interface BookService {
+    public void addBook(Book book);
+    public List<Book> findBookList();
+    public Book findBookById(int id);
+    public void updateBook(Book book);
+    public void removeBook(int id);
+    
+} 
